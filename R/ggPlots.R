d5db10b763d041d915d5058921acca83eef44234
Nima Hejazi <nima.hejazi7@gmail.com>
2016-03-15 16:49:09 -0700

    revising major errors

--- R/ggPlots.R
+++ R/ggPlots.R
@@ -1,11 +1,6 @@
-utils::globalVariables(c("aes", ".fitted", ".resid", "geom_point", ".stdresid",
-                       "geom_hline", "xlab", "ylab", "ggtitle", "stat_smooth",
-                       "geom_abline", ".cooksd", "geom_bar", ".hat", "theme",
-                       "scale_size_continuous", "z", "ord.x", "geom_point",
-                       "geom_ribbon", "lower", "upper", "geom_text", "label",
-                       "cens", "surv", "geom_step", "up", "low", "theme_bw",
-                       "group", "scale_colour_manual", "scale_colour_discrete",
-                       "scale_linetype_manual"))
+utils::globalVariables(c("aes", "geom_point","xlab", "ylab", "ggtitle", "theme",
+                       "geom_abline", "geom_text", "geom_ribbon","ord.x","z",
+                       "scale_size_continuous", "lower", "upper", "label"))
 
 #' Quantile-Quantile Plots with ggplot2
 #'
@@ -13,13 +8,18 @@ utils::globalVariables(c("aes", ".fitted", ".resid", "geom_point", ".stdresid",
 #'
 #' @param x A numeric vector of residuals from a generalized linear model.
 #' @param distribution The reference probability distribution for residuals.
+#' @param ... Any additional parameters to be passed to distribution functions.
+#' @param conf The confidence level to be used with confidence intervals.
+#' @param labels The names to be used when identifying points on the Q-Q plot.
+#' @param line.estimate Should quantiles be estimated, if so which quantiles?
 #'
-#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 ggplot aes geom_point xlab ylab ggtitle theme
+#' @importFrom ggplot2 geom_abline geom_text geom_ribbon scale_size_continuous
 #'
 #' @export qqPlot_gg
 #'
 #' @examples
-#' n <- 10; x1 <- rnorm(n); y1 <- rnorm(n)
+#' n <- 100; x1 <- rnorm(n); y1 <- rnorm(n);
 #' linmod <- lm(y1 ~ x1)
 #' x <- linmod$residuals
 #' qqPlot_gg(x)
@@ -50,7 +50,7 @@ qqPlot_gg <- function(x, distribution = "norm", ..., line.estimate = NULL,
   df$lower <- fit.value - zz * SE
 
   if(!is.null(labels)) {
-    df$label <- ifelse(df$ord.x > df$upper | df$ord.x < df$lower, labels[ord], "")
+    df$label <- ifelse(df$ord.x > df$upper | df$ord.x < df$lower,labels[ord],"")
   }
 
   p <- ggplot(df, aes(x = z, y = ord.x)) +
@@ -66,59 +66,77 @@ qqPlot_gg <- function(x, distribution = "norm", ..., line.estimate = NULL,
 }
 
 
+#####################################
+########### NEXT FUNCTION ###########
+#####################################
+
+utils::globalVariables(c("aes", ".fitted", ".resid", "geom_point", ".stdresid",
+                       "geom_hline", "xlab", "ylab", "ggtitle", "stat_smooth",
+                       "geom_abline", ".cooksd", "geom_bar", ".hat", "theme",
+                       "scale_size_continuous"))
+
 #' Linear Model Diagnostic Plots with ggplot2
 #'
 #' Produce standard diagnostic plots for linear models using ggplot2.
 #'
 #' @param model A linear model object produced by \code{lm()}.
 #'
-#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 ggplot aes geom_point stat_smooth geom_hline geom_abline
+#' @importFrom ggplot2 xlab ylab ggtitle geom_bar scale_size_continuous theme
 #' @importFrom gridExtra grid.arrange
 #'
 #' @export lmPlots_gg
 #'
 #' @examples
-#' n <- 10; x1 <- rnorm(n); y1 <- rnorm(n)
+#' n <- 100; x1 <- rnorm(n); y1 <- rnorm(n);
 #' linmod <- lm(y1 ~ x1)
 #' lmPlots_gg(linmod)
 
 lmPlots_gg <- function(model) {
-  p1 <- ggplot(model, aes(.fitted, .resid)) + geom_point()
-  p1 <- p1 + stat_smooth(method = "loess") +
-             geom_hline(yintercept = 0, col = "red", linetype = "dashed")
-  p1 <- p1 + xlab("Fitted values") + ylab("Residuals") +
-             ggtitle("Residual vs. Fitted")
+  p1 <- ggplot(model, aes(x = .fitted, y = .resid)) + geom_point() +
+        stat_smooth(method = "loess") +
+        geom_hline(yintercept = 0, col = "red", linetype = "dashed") +
+        xlab("Fitted vals") + ylab("Residuals") + ggtitle("Residual vs. Fitted")
 
-  p2 <- ggplot(model,aes(sample=.stdresid)) + geom_point(stat = "qq") +
+  p2 <- ggplot(model, aes(sample = .stdresid)) + geom_point(stat = "qq") +
         geom_abline() + xlab("Theoretical Quantiles") + ylab("Std. Residuals") +
         ggtitle("Gausian Q-Q")
 
-  p3 <- ggplot(model, aes(.fitted, sqrt(abs(.stdresid)))) +
-        geom_point(na.rm = TRUE)
-  p3 <- p3 + stat_smooth(method = "loess", na.rm = TRUE) + xlab("Fitted Value")
-  p3 <- p3 + ylab(expression(sqrt("|Std. residuals|")))
-  p3 <- p3 + ggtitle("Scale-Location")
+  p3 <- ggplot(model, aes(x = .fitted, y = sqrt(abs(.stdresid)))) +
+        geom_point(na.rm = TRUE) + stat_smooth(method = "loess", na.rm = TRUE) +
+        xlab("Fitted Value") + ylab(expression(sqrt("|Std. residuals|"))) +
+        ggtitle("Scale-Location")
 
-  p4 <- ggplot(model, aes(seq_along(.cooksd), .cooksd)) +
-        geom_bar(stat = "identity", position = "identity")
-  p4 <- p4 + xlab("Obs. No.") + ylab("Cook's distance")
-  p4 <- p4 + ggtitle("Cook's distance")
+  p4 <- ggplot(model, aes(x = seq_along(.cooksd), y = .cooksd)) +
+        geom_bar(stat = "identity", position = "identity") +
+        xlab("Obs. No.") + ylab("Cook's distance") + ggtitle("Cook's distance")
 
-  p5 <- ggplot(model, aes(.hat, .stdresid)) +
-        geom_point(aes(size = .cooksd), na.rm = TRUE)
-  p5 <- p5 + stat_smooth(method = "loess", na.rm = TRUE) + xlab("Leverage") +
-             ggtitle("Resid. vs Leverage") + ylab("Std. Resid.")
-  p5 <- p5 + scale_size_continuous("Cook's Distance", range = c(1,5))
-  p5 <- p5 + theme(legend.position = "none")
+  p5 <- ggplot(model, aes(x = .hat, y = .stdresid)) +
+        geom_point(aes(size = .cooksd), na.rm = TRUE) +
+        stat_smooth(method = "loess", na.rm = TRUE) + xlab("Leverage") +
+        ggtitle("Resid. vs Leverage") + ylab("Std. Resid.") +
+        scale_size_continuous("Cook's Distance", range = c(1,5)) +
+        theme(legend.position = "none")
 
-  p6 <- ggplot(model, aes(.hat, .cooksd)) + geom_point(na.rm = TRUE) +
+  p6 <- ggplot(model, aes(x = .hat, y = .cooksd)) + geom_point(na.rm = TRUE) +
         stat_smooth(method = "loess", na.rm = TRUE) + xlab("Leverage") +
         ylab("Cook's Distance") + ggtitle("Cook's dist. vs. Lev.") +
         geom_abline(slope = seq(0,3,0.5), color = "gray", linetype = "dashed")
 
-  return(grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2, ncol = 3))
+  return(gridExtra::grid.arrange(p1, p2, p3, p4, p5, p6, nrow = 2, ncol = 3))
 }
 
+
+#####################################
+########### NEXT FUNCTION ###########
+#####################################
+
+utils::globalVariables(c("aes", "geom_point", "geom_hline", "xlab", "ylab", 
+                         "ggtitle", "stat_smooth", "geom_abline", "geom_bar", 
+                         "geom_step", "theme", "scale_size_continuous", 
+                         "scale_colour_discrete", "scale_linetype_manual",
+                         "scale_colour_manual", "cens", "surv", "low", "up", 
+                         "theme_bw", "group"))
 
 #' Kaplan-Meier Plot with ggplot2
 #'
@@ -132,16 +150,26 @@ lmPlots_gg <- function(model) {
 #' @param xlab A label for the x-axis, defaults to "Time".
 #' @param ylab A label for the y-axis, defaults to "Survival".
 #' @param main A main label for the survival plot, no default.
+#' @param plot.cens Should the plot include the censored values?
+#' @param lty.est The number of survival curves to be generated from the model. 
+#' @param lty.ci The number of lines to be generated for confidence intervals. 
+#' @param cens.shape The shapes of points plotted, passed to \code{geom_point}
 #'
-#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 ggplot aes geom_point geom_hline xlab ylab ggtitle
+#' @importFrom ggplot2 stat_smooth geom_abline geom_bar geom_step theme 
+#' @importFrom ggplot2 scale_size_continuous scale_colour_discrete theme_bw 
+#' @importFrom ggplot2 scale_linetype_manual scale_colour_manual
 #' @importFrom survival survfit Surv
 #'
 #' @export survPlot_gg
 #'
 #' @examples
-#' hmohiv <- read.table("http://www.ats.ucla.edu/stat/r/examples/asa/hmohiv.csv", sep=",", header = TRUE)
-#' mini <- hmohiv[ID <= 5, ]
-#' s <- survfit(Surv(mini$time, mini$censor) ~ 1)
+#' time <- c(5,6,8,3,22)
+#' age <- c(46,35,30,30,36)
+#' drug <- c(0,1,1,1,0)
+#' censor <- c(1,0,1,1,1)
+#' survdat <- as.data.frame(cbind(time, age, drug, censor))
+#' s <- survfit(Surv(survdat$time, survdat$censor) ~ 1, conf.type = "none")
 #' survPlot_gg(s)
 
 survPlot_gg <- function(s, CI = "def", plot.cens = TRUE, surv.col = "gg.def",
---
+++ cran-comments.md
@@ -0,0 +1,9 @@
+## Resubmission
+This is a resubmission. In this version I have:
+
+* Revised most functions to resolve any outstanding warnings and/or errors.
+
+* Added a reference to the file LICENSE in the file DESCRIPTION.
+
+* 
+
