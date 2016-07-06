6cc753210db9431c147c5936aafe2e56e2996cf2
Nima Hejazi <nima.hejazi7@gmail.com>
2016-07-05 17:32:18 -0700

    fix startupMessage + update Description

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,4 @@
 .onAttach <- function(...) {
   packageStartupMessage("nima: Nima Hejazi's Miscellaneous R Tools")
-  packageStartupMessage("Version: ", utils::packageDescription('nima')$Version)
-  packageStartupMessage("Package created on ", utils::packageDescription("nima")$Date, "\n")
-  if (utils::packageDescription("nima")$RemoteType == "github" & utils::packageDescription("nima")$RemoteRef == "develop") {
-    packageStartupMessage("This is the development version. \n Make sure to update frequently.")
-  }
+  packageStartupMessage("Version: ", utils::packageDescription("nima")$Version)
 }
