bd90d68b4da89a3d5ec957ba84ae49fcf4064823
Nima Hejazi <nh@nimahejazi.org>
2018-01-18 22:51:58 -0800

    add hex

--- hex/sticker.R
+++ hex/sticker.R
@@ -5,8 +5,8 @@ img_path <- here("hex", "nima_v2.png")
 sticker_path <- here("hex", "nima_sticker.pdf")
 
 sticker(img_path, package = "nima",
-        p_color = "#242A30", p_size = 5, p_x = 1, p_y = 1.77,
-        s_x = 1, s_y = 0.975, s_width = 0.6,
+        p_color = "#242A30", p_size = 5, p_x = 1, p_y = 0.3,
+        s_x = 1, s_y = 1.1, s_width = 0.6,
         h_fill = "#ffffff", h_color = "#1881C2",
         filename = sticker_path)
 
