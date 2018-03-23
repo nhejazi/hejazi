1e129331dd21e261e08618b1f87d04c1bf19b5f2
Nima Hejazi <nh@nimahejazi.org>
2018-03-23 00:27:33 -0700

    catch realization of #14

--- tests/testthat/test-attribute_names.R
+++ tests/testthat/test-attribute_names.R
@@ -6,9 +6,8 @@ n <- 100
 test_that("attrnames finds the names of the attributes of a matrix", {
   x <- matrix(seq_len(n), ncol = p)
   colnames(x) <- LETTERS[seq_len(p)]
-  expect_identical(
-    attrnames(x),
-    c("dim", "dimnames")
+  expect_true(
+    identical(attrnames(x), c("dim", "dimnames"))
   )
 })
 
@@ -16,8 +15,7 @@ test_that("attrnames finds the names of the attributes of a data frame", {
   x <- matrix(seq_len(n), ncol = p)
   colnames(x) <- LETTERS[seq_len(p)]
   y <- data.frame(x)
-  expect_identical(
-    attrnames(y),
-    c("names", "row.names", "class")
+  expect_true(
+    identical(attrnames(y), c("names", "row.names", "class"))
   )
 })
